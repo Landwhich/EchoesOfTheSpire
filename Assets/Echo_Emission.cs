e7556a08a96502d64e132ac717b68d6d238bb941
Landwhich <will.f.richards@gmail.com>
2025-03-05 22:12:08 -0500

    Added First Person Camera and EchoRay functionality

--- Assets/Echo_Emission.cs
+++ Assets/Echo_Emission.cs
@@ -2,17 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
 public class Echo_Emission : MonoBehaviour
 {
+public Camera firstPersonCamera;
+public Transform echoRayOrigin;
+public float range;
+public float rate;
+public float echoDuration;
+
+LineRenderer echoRay;
+private float rayTimer;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        echoRay = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rayTimer += Time.deltaTime;
+        if(Input.GetButtonDown("Fire1") && rayTimer > rate){
+            rayTimer = 0;
+            echoRay.SetPosition(0, echoRayOrigin.position);
+            Vector3 origin = firstPersonCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+            if(Physics.Raycast(origin, firstPersonCamera.transform.forward, out hit, range))
+            {
+                echoRay.SetPosition(1, hit.point);
+            }
+            else
+            {
+                echoRay.SetPosition(1, origin + (firstPersonCamera.transform.forward * range));
+            }
+            StartCoroutine(ShootRay());
+        }
+    }
+
+    IEnumerator ShootRay()
+    {
+        echoRay.enabled = true;
+        yield return new WaitForSeconds(echoDuration);
+        echoRay.enabled = false;
     }
 }
