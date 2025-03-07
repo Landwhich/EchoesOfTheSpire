561988463b4d39abbc2a70f396a400cc80380038
Landwhich <will.f.richards@gmail.com>
2025-03-06 19:37:14 -0500

    1st person POV and Folder layout
    Fixed Folder Hierarchy and Improved First Person Functionality with Pitch and Yaw mapped to the mouse

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -8,8 +8,10 @@ public class PlayerControls : MonoBehaviour
     public float floatHeight = 0.05f;  // Hover range
     public float floatSpeed = 1f;  // Speed of up/down movement (slow oscillation)
     public float moveSpeed = 5f;  // Speed of movement along X and Z axes
+    public float mouseSens = 100f;
 
     public Transform playerBody;  // Reference to the player's body (for rotation, if needed)
+    private float xRotation = 0f;
     private CharacterController controller;  // Character controller for movement
 
     private Vector3 originalPosition;  // Store the original position for floating effect
@@ -18,16 +20,26 @@ public class PlayerControls : MonoBehaviour
     {
         controller = GetComponent<CharacterController>();
         originalPosition = transform.position;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
         // Player Movement with WASD
+        float pitch = mouseSens * Input.GetAxis("Mouse Y") * Time.deltaTime;
+        float yaw = mouseSens * Input.GetAxis("Mouse X") * Time.deltaTime;
+
         float moveX = Input.GetAxis("Horizontal") * moveSpeed; // Left/Right (A/D)
         float moveZ = Input.GetAxis("Vertical") * moveSpeed; // Forward/Backward (W/S)
 
-        // Calculate movement direction
+        // Calculate movement and rotation direction
         Vector3 move = transform.right * moveX + transform.forward * moveZ;
+
+        // apply rotation
+        playerBody.Rotate(Vector3.up * yaw);
+        xRotation -= pitch;
+        xRotation = Mathf.Clamp(xRotation, -45f, 45f);
+        Camera.main.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
         // Apply movement
         controller.Move(move * Time.deltaTime);  // Apply movement with CharacterController
