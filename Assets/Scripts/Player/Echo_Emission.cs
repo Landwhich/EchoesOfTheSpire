15d0d9740b2f3da32af8bd56acf02c96846fc29c
Landwhich <will.f.richards@gmail.com>
2025-03-27 23:32:26 -0400

    added some UI

--- Assets/Scripts/Player/Echo_Emission.cs
+++ Assets/Scripts/Player/Echo_Emission.cs
@@ -1,5 +1,7 @@
 using System.Collections;
+// using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(LineRenderer))]
 public class Echo_Emission : MonoBehaviour
@@ -7,17 +9,24 @@ public class Echo_Emission : MonoBehaviour
     public Camera firstPersonCamera;
     public Transform echoRayOrigin;
     public float range;
-    public float rate;
+    public float rate = 2f;
     private float echoTimer;
     private float echoDuration = 0.0004f;
     private float rayTimer;
     public Material echoMaterial;
+    public Image echoCDBar;
+
     // public Transform echoCenter;
 
+    void Start()
+    {
+        // cooldownPanel.sizeDelta = emptyCD;
+    }
     // Start is called before the first frame update
     void Awake()
     {
         echoMaterial.SetFloat("_EchoDuration", 0);
+        echoCDBar.fillAmount = 0f;
         // echoRay = GetComponent<LineRenderer>();
         // rippleMaterial = GetComponent<Renderer>().material; // Assuming the script is attached to the object with the ripple material
     }
@@ -27,6 +36,7 @@ public class Echo_Emission : MonoBehaviour
     {
         if (echoTimer > 0.0) {echoTimer -= echoDuration;}
         rayTimer += Time.deltaTime;
+        echoCDBar.fillAmount += Time.deltaTime / 3;
         if (Input.GetButtonDown("Fire1") && rayTimer > rate)
         {
             rayTimer = 0;
@@ -34,6 +44,9 @@ public class Echo_Emission : MonoBehaviour
 
             Vector3 origin = firstPersonCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
             RaycastHit hit;
+            
+
+
 
             if (Physics.Raycast(origin, firstPersonCamera.transform.forward, out hit, range))
             {
@@ -42,11 +55,12 @@ public class Echo_Emission : MonoBehaviour
                 echoTimer = 1f;
 
                 // echoRay.SetPosition(1, hit.point);
-
+                echoCDBar.fillAmount = 0;
                 Debug.Log("Ray hit: " + hit.collider.gameObject.name);
             }
             else
             {
+                
                 // echoRay.SetPosition(1, origin + (firstPersonCamera.transform.forward * range));
                 Debug.Log("Ray did not hit anything.");
             }
