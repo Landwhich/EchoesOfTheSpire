ae9d2d4095361bee9a864c8dc7dd9570a1239673
Rachel <99687457+xRaquelx@users.noreply.github.com>
2025-04-07 12:46:29 -0400

    button interaction
    changed it so that its either 1 or 2. 1 spawns enemy, 2 spawns health

--- Assets/Scripts/ButtonEnemyInteraction.cs
+++ Assets/Scripts/ButtonEnemyInteraction.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 public class ButtonInteraction : MonoBehaviour
 {
     public GameObject EnemyModel;
+    public GameObject HealthModel;
     public Transform spawn;
-    public float interactionRange = 5f;
+    public float interactionRange = 16f;
     public string playerTag = "Player";
 
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag(playerTag))
         {
-            if(Input.GetKeyDown(KeyCode.E))
-            {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
                 SpawnEnemy();
-            }
+            else if (Input.GetKeyDown(KeyCode.Alpha2))
+                SpawnHealth();
         }
     }
 
@@ -26,6 +27,16 @@ public class ButtonInteraction : MonoBehaviour
         {
             Instantiate(EnemyModel, spawn.position, spawn.rotation);
             Debug.Log("Enemy spawn");
+        }
+        Destroy(gameObject);
+    }
+
+    private void SpawnHealth()
+    {
+        if (HealthModel != null && spawn != null)
+        {
+            Instantiate(HealthModel, spawn.position, spawn.rotation);
+            Debug.Log("Health spawn");
         }
         Destroy(gameObject);
     }
